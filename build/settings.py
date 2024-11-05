c0d8f3071676479d426ec9838d284bdfeb5d2678
vvarve <vvarve@gmail.com>
2024-11-05 02:40:02 -0500

    Stage 02: add docker-compose and postgresql

--- build/settings.py
+++ build/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'build.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql', # -> connecting db postgresql
+        'NAME': 'appointment_db',
+        'USER': 'appointment_user',
+        'PASSWORD': '1234sql',
+        'HOST': 'appointment_db',
+        'PORT': 5432,
     }
 }
 
